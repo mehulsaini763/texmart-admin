04261e17f38ba72c822f647a8060cd527675f1e5
mehulsaini763 <mehulsaini763@gmail.com>
2024-10-26 20:22:20 +0530

    -

--- app/api/stores/[storeId]/webhook/route.js
+++ app/api/stores/[storeId]/webhook/route.js
@@ -5,11 +5,14 @@ export const POST = async (req, { params }) => {
   try {
     const { storeId } = params;
     const data = await req.json();
+    console.log(data);
 
     const paymentId = data.payload.payment_link.entity.id;
     const status = data.payload.payment_link.entity === 'paid';
+    console.log(status);
 
     const order = await Order.findOneAndUpdate({ storeId, paymentId }, { isPaid: status });
+    console.log(order);
 
     return NextResponse.json({ success: true, message: 'Order Paid' }, { status: 200 });
   } catch (error) {
